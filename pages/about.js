097cb56275697cf7f05a0ab60254a7dfbeb6a5ac
hscanlan <harry.scanlan@gmail.com>
2025-03-18 10:20:57 -0400

    listings

--- pages/about.js
+++ pages/about.js
@@ -4,7 +4,7 @@ import ListingDetails from "@/components/ListingDetails";
 import PageHeader from "@/components/PageHeader";
 
 export async function getStaticProps() {
-  const res = await fetch("/api/listings/1206363");
+  const res = await fetch("https://web422-as1-navy.vercel.app/api/listings/1206363");
   const data = await res.json();
   return { props: { listing: data } };
 }
